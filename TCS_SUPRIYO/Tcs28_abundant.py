861aa64783e034cdd446475c85151d4f10008768
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2022-07-31 19:29:29 +0530

    Update Tcs28_abundant.py

--- TCS_SUPRIYO/Tcs28_abundant.py
+++ TCS_SUPRIYO/Tcs28_abundant.py
@@ -1,4 +1,8 @@
-n= int(input("Enter a num: "))
+# Example 1:
+# Input: 18
+# Output: Abundant Number
+# Explanation: Divisors of 18 are 1,2,3,6,9. 1+2+3+6+9=21, Since 21 is greater than 18, 18 is an abundant number.
+# n= int(input("Enter a num: "))
 
 temp=n
 sum=0
