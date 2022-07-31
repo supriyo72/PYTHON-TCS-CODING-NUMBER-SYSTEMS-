d93e0a2acd6aedb9e9d2516df1c8ec46353b5d60
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2022-07-31 18:40:59 +0530

    Add files via upload

---
+++ TCS_SUPRIYO/Tcs26_lcm.py
@@ -0,0 +1,14 @@
+num1= int(input("Enter num1: "))
+num2= int(input("Enter num2: "))
+
+if num1 > num2:
+    min= num2
+else:
+    min=num1
+    
+for i in range(1,min+1):
+    if(num1%i==0 and num2%i==0):
+        hcf=i
+        
+lcm= num1*num2/hcf
+print(lcm)
