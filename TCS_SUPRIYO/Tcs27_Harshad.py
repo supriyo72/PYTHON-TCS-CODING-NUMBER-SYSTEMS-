85962b888a529a3af1560a6e7793bcb51ccfc7d3
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2022-07-31 19:30:30 +0530

    Update Tcs27_Harshad.py

--- TCS_SUPRIYO/Tcs27_Harshad.py
+++ TCS_SUPRIYO/Tcs27_Harshad.py
@@ -1,3 +1,8 @@
+# Example 1:
+# Input: 378
+# Output: Yes it is a Harshad number.
+# Explanation: 3+7+8=18. 378 is divisible by 18. Therefore 378 is a harshad number.
+
 n=int(input("Enter a number: "))
 
 temp=n
